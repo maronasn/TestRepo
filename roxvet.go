b21a5ffe332c35be9b257e26e156c894f8e39047
Natalie Maronas <73603131+maronasn@users.noreply.github.com>
2022-10-02 22:20:34 -0400

    Add files via upload

---
+++ roxvet.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/stackrox/rox/tools/roxvet/analyzers/dontprintferr"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/filepathwalk"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/godoccapitalizationmismatch"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/importpackagenames"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/needlessformat"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/protoclone"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/regexes"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/storeinterface"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/uncheckederrors"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/uncheckedifassign"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/unusedroxctlargs"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/validateimports"
+	"github.com/stackrox/rox/tools/roxvet/analyzers/verifylicenseheader"
+	"golang.org/x/tools/go/analysis/unitchecker"
+)
+
+func main() {
+	unitchecker.Main(
+		godoccapitalizationmismatch.Analyzer,
+		dontprintferr.Analyzer,
+		storeinterface.Analyzer,
+		uncheckederrors.Analyzer,
+		needlessformat.Analyzer,
+		regexes.Analyzer,
+		uncheckedifassign.Analyzer,
+		protoclone.Analyzer,
+		unusedroxctlargs.Analyzer,
+		filepathwalk.Analyzer,
+		validateimports.Analyzer,
+		importpackagenames.Analyzer,
+		verifylicenseheader.Analyzer
+	)
+}
